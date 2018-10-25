ba65ba2c217c1433dd7495890aebd12f83570e4c
= <ec2-user@ip-172-31-41-242.eu-west-1.compute.internal>
2018-10-25 10:06:50 +0000

    blocking

---
+++ creditcard-lib/creditcard-app/src/main/java/pl/jkan/creditcard/CreditCard.java
@@ -0,0 +1,23 @@
+package pl.jkan.creditcard;
+
+class CreditCard {
+    
+    private boolean blocked = false;
+    
+    public void assignLimit(double money) {
+        
+    }
+    
+    public double getLimit() {
+        return 2000;
+    }
+    
+    public void block() {
+        this.blocked = true;
+    }
+    
+    public boolean isBloked(){
+    return blocked= true;    
+    
+    
+}
